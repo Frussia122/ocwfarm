ba7a92162a8e47a1db67597a4a93f58fa9af9bd6
Amidgard <amidgard@list.ru>
2024-01-18 13:43:23 +0300

    feature: added loginPage

---
+++ client/src/app/App.tsx
@@ -0,0 +1,13 @@
+import { LoginPage } from "pages/Login";
+import "./style.css"
+
+function App() {
+  return (
+    <div>
+      <LoginPage />
+      asdd
+    </div>
+  );
+}
+
+export default App;
